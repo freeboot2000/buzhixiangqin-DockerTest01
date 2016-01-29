e7f6f5212fbd2eaff9a372eb12ed75122a8f25ef
yishan.sun <yishan.sun@alipay.com>
2016-01-29 16:31:43 +0800

    docker new build

--- dockerfile
+++ dockerfile
@@ -6,34 +6,38 @@ RUN apt-get update
 
 # Install JDK
 # RUN apt-get install -y default-jdk
-# ADD jdk-8u71-linux-x64.gz /usr/lib/jvm
+RUN wget --no-cookies --no-check-certificate --header "Cookie:gpw_e24=http%3a%2f%2fwww.oracle.com%2ftechnetwork%2fjava%2fjavase%2fdownloads%2fjdk8-downloads-2133151.ht
+ml;oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u71-b15/jdk-8u71-linux-x64.tar.gz
+RUN find -name jdk-8u71-linux-x64.tar.gz
+RUN cd /usr/lib \
+mkdir jvm
+
+RUN tar -xzf ./jdk-8u71-linux-x64.tar.gz -C /usr/lib/jvm/
+
+#ADD jdk-8u71-linux-x64.gz /usr/lib/jvm
 
 # Install subversion
 RUN apt-get install -y subversion
 
 # Install maven
-#ADD apache-maven-3.3.9-bin.tar.gz /usr/local/
-#ADD apache-maven-3.3.9-bin.tar.gz /usr/local/004
+RUN wget http://mirrors.hust.edu.cn/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+RUN tar -xzf ./jdk-8u71-linux-x64.tar.gz -C /usr/local/
+
 
 #update code and build
 RUN svn checkout svn://115.29.113.90/server_patch_0816 --username steven --password sunbbs99
 
 #RUN ls /usr/local/
 #RUN tar -xzf /usr/local/apache-maven-3.3.9-bin.tar.gz
-#Env JAVA_HOME /usr/lib/jvm/jdk1.8.0_71
-#ENV M2_HOME /usr/local/apache-maven-3.3.9
-#ENV PATH $PATH:$M2_HOME/bin
-#:$JAVA_HOME/bin
+Env JAVA_HOME /usr/lib/jvm/jdk1.8.0_71
+ENV M2_HOME /usr/local/apache-maven-3.3.9
+ENV PATH $PATH:$M2_HOME/bin:$JAVA_HOME/bin
 
 
 ADD mavenbuild.sh .
-#RUN sh mavenbuild.sh
+RUN sh mavenbuild.sh
 
 #RUN cd /usr/local/tomcat/server_patch_0816/target/ \
 RUN ls /usr/local/tomcat/server_patch_0816/target/
 
-#RUN cp /usr/local/tomcat/server_patch_0816/target/SpringMVC.war /usr/local/tomcat/webapps/
-
-#RUN ls /usr/local/tomcat/bin/
-
-EXPOSE 8080
+RUN cp /usr/local/tomcat/server_patch_0816/target/SpringMVC.war /usr/local/tomcat/webapps/
--- mavenbuild.sh
+++ mavenbuild.sh
@@ -1,21 +1,17 @@
 cd /usr/local/tomcat/server_patch_0816
 
-mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/alipay-sdk-java20160119120053.jar  -DgroupId=alipay  -DartifactId=alipay-sdk-java -
-Dversion=20160119120053 -Dpackaging=jar
+mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/alipay-sdk-java20160119120053.jar  -DgroupId=alipay  -DartifactId=alipay-sdk-java -Dversion=20160119120053 -Dpackaging=jar
 
 mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/axis.jar  -DgroupId=axis  -DartifactId=axis -Dversion=1.0.0 -Dpackaging=jar
 
-mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/commons-discovery-0.2.jar  -DgroupId=commons  -DartifactId=commons-discovery -Dvers
-ion=0.2 -Dpackaging=jar
+mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/commons-discovery-0.2.jar  -DgroupId=commons  -DartifactId=commons-discovery -Dversion=0.2 -Dpackaging=jar
 
 mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/jaxrpc.jar  -DgroupId=jaxrpc  -DartifactId=jaxrpc -Dversion=1.0.0 -Dpackaging=jar
 
-mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/mailapi_1_3_1.jar  -DgroupId=mailapi  -DartifactId=mailapi -Dversion=1.3.1 -Dpackag
-ing=jar
+mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/mailapi_1_3_1.jar  -DgroupId=mailapi  -DartifactId=mailapi -Dversion=1.3.1-Dpackaging=jar
 
 mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/saaj.jar  -DgroupId=saaj  -DartifactId=saaj -Dversion=1.0.0 -Dpackaging=jar
 
-mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/wsdl4j-1.5.1.jar  -DgroupId=wsdl4j  -DartifactId=wsdl4j -Dversion=1.5.1 -Dpackaging
-=jar
+mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/wsdl4j-1.5.1.jar  -DgroupId=wsdl4j  -DartifactId=wsdl4j -Dversion=1.5.1 -Dpackaging=jar
 
-mvn package
+mvn clean package
