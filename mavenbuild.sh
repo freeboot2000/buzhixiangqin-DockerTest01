0ed48cc705377903bb8edf64b8c1d79fe54fec9c
yishan.sun <yishan.sun@alipay.com>
2016-01-30 21:34:18 +0800

    fix mailapi

--- mavenbuild.sh
+++ mavenbuild.sh
@@ -8,7 +8,7 @@ mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/common
 
 mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/jaxrpc.jar  -DgroupId=jaxrpc  -DartifactId=jaxrpc -Dversion=1.0.0 -Dpackaging=jar
 
-mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/mailapi_1_3_1.jar  -DgroupId=mailapi  -DartifactId=mailapi -Dversion=1.3.1-Dpackaging=jar
+mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/mailapi_1_3_1.jar  -DgroupId=mailapi  -DartifactId=mailapi -Dversion=1.3.1 -Dpackaging=jar
 
 mvn install:install-file  -Dfile=/usr/local/tomcat/server_patch_0816/libs/saaj.jar  -DgroupId=saaj  -DartifactId=saaj -Dversion=1.0.0 -Dpackaging=jar
 
